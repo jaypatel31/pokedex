58d590e47bb1ddd29c6141c3e264d338ca0b574e
Jay Patel <jaypatel32157@gmail.com>
2021-06-10 13:29:47 +0530

    Minor Fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import "./Header.css";
+import {AiFillGithub} from "react-icons/ai";
+import { BiWorld } from "react-icons/bi";
 
 function Header() {
 	return (
 		<div className="header-container">
 			<div className="navbar">
 				<ul>
-					<li><a href="#">A</a></li>
+					<li><a href="http://github.com/jaypatel31" target="_blank"><AiFillGithub/></a></li>
 					<li><a href="#">POKEDEX</a></li>
-					<li><a href="#">C</a></li>
+					<li><a href="http://pateljay.me"><BiWorld/></a></li>
 				</ul>
 			</div>
 		</div>
